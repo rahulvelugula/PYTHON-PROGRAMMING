c6a9cafca779cadecf83ef45520f92957f08eb8e
Velugula Rahul <rahulvelugula2007@gmail.com>
2026-05-09 09:09:48 +0530

    Added bookstore catalog exercise

---
+++ 01_Getting_Started_Exercises/01_bookstore_catalog.py
@@ -0,0 +1,23 @@
+"""
+Question:
+Create a simple bookstore catalog.
+Read the title and author of two books from the user
+and display their details.
+"""
+
+# First book details
+title1 = input()
+author1 = input()
+
+# Second book details
+title2 = input()
+author2 = input()
+
+# Display details
+print("First Book Details:")
+print("Book Title:", title1)
+print("Author:", author1)
+
+print("Second Book Details:")
+print("Book Title:", title2)
+print("Author:", author2)
