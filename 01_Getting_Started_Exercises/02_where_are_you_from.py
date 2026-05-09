af1a9a9f5b1c29d58bf90215e01a8e9390125c3a
Velugula Rahul <rahulvelugula2007@gmail.com>
2026-05-09 10:13:24 +0530

    Added where are you from exercise

---
+++ 01_Getting_Started_Exercises/02_where_are_you_from.py
@@ -0,0 +1,12 @@
+"""
+Question:
+Read a person's country of origin and current location,
+then display the information in sentence format.
+"""
+
+country = input()
+location = input()
+
+print(f"I come from {country}.")
+print(f"I live in {location}.")
+print(f"It's nice to meet someone from {country}!")
