9b2f39787044cd2e46acb29ee3d5750c26a70ec1
Velugula Rahul <rahulvelugula2007@gmail.com>
2026-05-10 08:38:08 +0530

    Added favorite song exercise

---
+++ 01_Getting_Started_Exercises/03_favorite_song.py
@@ -0,0 +1,12 @@
+"""
+Question:
+Read a person's favorite song and favorite singer,
+then display the information in sentence format.
+"""
+
+song = input()
+singer = input()
+
+print(f"My favorite song is {song}.")
+print(f"It is sung by {singer}.")
+print(f"I can listen to {song} by {singer} all day!")
