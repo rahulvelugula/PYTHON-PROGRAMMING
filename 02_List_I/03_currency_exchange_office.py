956566dc2877f63e0d3ee0434042fcf3b296f8ab
Velugula Rahul <rahulvelugula2007@gmail.com>
2026-05-12 18:23:12 +0530

    Added currency exchange program using lists and list operations

---
+++ 02_List_I/03_currency_exchange_office.py
@@ -0,0 +1,30 @@
+"""
+Question:
+Maintain a list of available currencies
+and currencies to order.
+
+If the requested currency is available:
+- inform the customer
+- remove it from available currencies
+- add it to currencies to order
+
+Otherwise:
+- inform the customer
+- add it to currencies to order
+"""
+
+available_currencies = input().split(", ")
+order_currencies = input().split(", ")
+
+currency = input()
+
+if currency in available_currencies:
+    print(f"Yes, {currency} are available.")
+    available_currencies.remove(currency)
+    order_currencies.append(currency)
+else:
+    print(f"Sorry, {currency} are not available.")
+    order_currencies.append(currency)
+
+print("Updated available currencies:", available_currencies)
+print("Updated currencies to order:", order_currencies)
