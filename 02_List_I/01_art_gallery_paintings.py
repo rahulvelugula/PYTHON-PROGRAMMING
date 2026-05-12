e5366c66771aaa02363449991ede57422e251f49
Velugula Rahul <rahulvelugula2007@gmail.com>
2026-05-12 18:20:57 +0530

    Added painting availability checker using lists and if-else

---
+++ 02_List_I/01_art_gallery_paintings.py
@@ -0,0 +1,22 @@
+"""
+Question:
+Maintain a list of paintings in an art gallery.
+
+Check whether the painting requested by the customer
+is available in the gallery or not.
+"""
+
+paintings = [
+    "Starry Night",
+    "The Mona Lisa",
+    "The Last Supper",
+    "The Scream",
+    "Girl with a Pearl Earring"
+]
+
+painting = input()
+
+if painting in paintings:
+    print(f'Yes, we have "{painting}" available for purchase!')
+else:
+    print(f'Sorry, we dont have "{painting}" right now.')
