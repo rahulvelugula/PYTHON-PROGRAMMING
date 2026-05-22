9d272ec6c20ab5d6b62b44efab827cff004307a0
Rahul Velugula <rahulvelugula2007@gmail.com>
2026-05-22 22:47:09 +0530

    Added majority element problem using nested loops

--- 03_List_II/05_majority_element.py
+++ 03_List_II/05_majority_element.py
@@ -5,22 +5,15 @@ The majority element appears more than n/2 times.
 
 """
 
-#Read list of numbers
 nums = list(map(int, input().split()))
-
 majority = 0
 
-# Count frequency of each element
 for i in nums:
-
     count = 0
-
     for j in nums:
-
         if i == j:
             count += 1
 
-    # Check majority condition
     if count > len(nums) // 2:
         majority = i
         break
