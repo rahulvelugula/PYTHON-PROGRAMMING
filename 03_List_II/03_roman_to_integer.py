a12373af899e8e188cffbd5ecae2deda86c9b107
Rahul Velugula <rahulvelugula2007@gmail.com>
2026-05-23 18:56:05 +0530

    Added Roman to Integer conversion using dictionaries and loops

--- 03_List_II/03_roman_to_integer.py
+++ 03_List_II/03_roman_to_integer.py
@@ -4,7 +4,6 @@ Convert a Roman numeral into an integer.
 
 """
 
-# Roman numeral values
 roman = {
     'I': 1,
     'V': 5,
@@ -14,21 +13,11 @@ roman = {
     'D': 500,
     'M': 1000
 }
-
-# Read Roman numeral
 s = input()
-
 total = 0
-
-#Traverse the string
 for i in range(len(s)):
-
-    # If current value is smaller than next value,
-    #subtract it
     if i < len(s) - 1 and roman[s[i]] < roman[s[i + 1]]:
         total -= roman[s[i]]
-
-    # Otherwise add it
     else:
         total += roman[s[i]]
 
