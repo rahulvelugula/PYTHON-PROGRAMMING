daf5df30067ba8e643275d3ccbdb0a1ef975fb13
Rahul Velugula <rahulvelugula2007@gmail.com>
2026-05-23 18:56:56 +0530

    Added contains duplicate problem using nested loops and lists

--- 03_List_II/02_contains_duplicate.py
+++ 03_List_II/02_contains_duplicate.py
@@ -5,16 +5,12 @@ Return True if any value appears more than once,
 otherwise return False.
 
 """
-
-#Read list of numbers
 nums = list(map(int, input().split()))
-
 duplicate = False
 
 # Compare every element with remaining elements
 for i in range(len(nums)):
     for j in range(i + 1, len(nums)):
-
         #Duplicate found
         if nums[i] == nums[j]:
             duplicate = True
